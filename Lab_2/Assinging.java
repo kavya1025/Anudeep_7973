f921666e98e90ac2116a85fb85ffdc7fe3862d5a
kavya1025 <125475780+kavya1025@users.noreply.github.com>
2024-06-11 14:59:01 +0530

    Add files via upload

---
+++ Lab_2/Assinging.java
@@ -0,0 +1,19 @@
+package Lab_2;
+
+public class Assinging {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=10;
+		int y=5;
+		char name='d';
+		System.out.println("name: " + name);
+		int total;
+		total= x+y;//addition
+		System.out.println("addittion: " + total);
+		total= x-y;//subtraction
+		System.out.println("subtraction: "+total);
+
+	}
+
+}
---
+++ Lab_2/Statements.java
@@ -0,0 +1,18 @@
+package Lab_2;
+
+public class Statements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String login="deekshitha";
+		if(login=="deekshitha") {
+			System.out.println("user is valid");
+		}
+		else 
+		{
+			System.out.println("user is not valid");
+		}
+		
+	}
+
+}
